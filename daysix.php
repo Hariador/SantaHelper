7a1d676ca79f025e447b1424a326ab92a96408d3
Jonathan Reichert <Jonathan Reichert>
2015-12-07 21:48:51 -0600

    Day six A and B complete

--- daysix.php
+++ daysix.php
@@ -18,35 +18,26 @@ for($x = 0; $x <= 999; $x++) {
 while($line = fgets($input)){
 	printf($line);
 	$off_set = 1;
-	$light_status = 0;
+	$light_status = -1;
 	$commands = explode(" ",$line);
 	if($commands[0] == "toggle") {
-		$off_set = 0;
+		$off_set = 0 ;
 	}
 	else {
 		if($commands[1] == 'on')
 			$light_status = 1;
 	}
-	$start_coor = explode(',',$commands[1 + $off_set]);
-	$stop_coor = explode(',',$commands[3 + $off_set]);
-	$area = ($stop_coor[0] - $start_coor[0] + 1) * ($stop_coor[1] - $start_coor[1] + 1);
+	$start_coor = explode(',',trim($commands[1 + $off_set]));
+	$stop_coor = explode(',',trim($commands[3 + $off_set]));
+	// test cod
+	if($off_set == 0)
+		$light_status = 2;
 	for($x = $start_coor[0]; $x <= $stop_coor[0]; $x++) {
 		for($y = $start_coor[1]; $y <= $stop_coor[1]; $y++) {
-			if($off_set == 0) {
-				$lights[$x][$y] =  $lights[$x][$y] ^ 1;
+				$lights[$x][$y] = $light_status + $lights[$x][$y];
+			if($lights[$x][$y] < 0)
+				$lights[$x][$y] = 0;
 			}
-			else {
-				$lights[$x][$y] = $light_status;
-				$sum = 0 ;
-				for($x = 0; $x <= 999; $x++) {
-					for($y = 0; $y<=999; $y++){
-						$sum = $sum + $lights[$x][$y] ;
-					}
-				}
-
-				printf("Area: " . $area. " Lights Lite: " . $sum . "\n");
-			}
-		}
 	}
 }
 $sum = 0 ;
